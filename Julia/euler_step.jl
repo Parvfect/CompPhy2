bbdcd90fbf88b22ca523f5fec6cffa99c778074a
Parvfect <parvagrw02@gmail.com>
2021-10-08 15:59:10 +0100

    looking fine

---
+++ Julia/euler_step.jl
@@ -0,0 +1,13 @@
+
+function euler_step(vec::Vector, dt)
+    # Takes in a vector and returns that times dt
+    return vec*dt
+
+
+y = [0.0, 0.0]
+y_arr = 
+
+# Perform euler step
+@time for dt in 0:t
+
+end
