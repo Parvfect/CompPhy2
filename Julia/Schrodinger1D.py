c0ac913b24da150bca14c82064603897fa90bc65
Parvfect <parvagrw02@gmail.com>
2021-10-18 12:04:45 +0100

    Suck it

--- Julia/Schrodinger1D.py
+++ Julia/Schrodinger1D.py
@@ -8,3 +8,11 @@ plt.rc('savefig', dpi=300)
 import numpy as np
 
 from numba import jit, njit
+
+dx = 0.02
+x = np.arange(0, 10, dx)
+
+
+kx = 0.1
+m = 1
+sigma = 0.1
--- Julia/schrodinger.jl
+++ Julia/schrodinger.jl
@@ -1,7 +1,6 @@
 using Plots
 using LinearAlgebra
 
-e = MathConstants.e
 
 function Schrodinger1D(U, E, A2, B2)
     #= U - Potentials of both states, E - Energy of Wavefunction, A2 & B2 - Boundary Conditions, m : mass
@@ -15,16 +14,18 @@ function Schrodinger1D(U, E, A2, B2)
     # Calculating constants
     k1 = sqrt(complex(2* (E - U[1])))/6.626
     k2 = sqrt(complex(2* (E - U[2])))/6.626
+   
     k1k2 = k1 + k2
     k1_k2 = k1 - k2
 
     # Forming matrix
-    T = [k1k2*e^(-im*k1_k2) k1_k2*e^(-im*k1k2) ; k1_k2*e^(im*k1k2) k1k2*e^(im*k1_k2)]
-    Bc2 = [A2, B2] / 2k1
+    T = 1/2k1 * 
+    [k1k2*exp(-im*k1_k2) k1_k2*exp(-im*k1k2) ; k1_k2*exp(im*k1k2) k1k2*exp(im*k1_k2)]
+    Bc2 = [A2, B2]
     Bc1 = [A1, B1]
 
     # Performing Matrix Multiplication and extracting values
-    Bc1 = T * Bc2  
+    Bc1 = T * Bc2 
     A1 = Bc1[1]
     B1 = Bc1[2]
 
@@ -32,25 +33,37 @@ function Schrodinger1D(U, E, A2, B2)
 
 end
 
-#=
-function Ψ()
+
+function Ψ(A, B, k, x)
+    return A*exp(im * k* x) + B * exp(- im * k* x) 
 end
-=#
+
+function probabilityAmplitude(v)
+    #= Finds the Probability Amplitude of the Observable/Vector =#
+    return dot(v, conj(v))
+end
 
 function plotWaveFunction1D(Bc1, Bc2, k1, k2)
     # Plots the Wavefunction of an electron incident on the boundary in 1d """ 
     dx = 0.02
-    grid1 = 0:dx:5
-    grid2 = 5:dx:10
-    psi1 = []
-    psi2 = []
-    a = 100.0
+    grid = 0:dx:10
+    psi = complex(zeros(length(grid)))
     
-    append!(psi1, Bc1[1]e^(im*k1*grid1) + Bc1[2]e^(-im*k1*i))
-    append!(psi2, Bc2[1]e^(im*k2*grid2) + Bc2[2]e^(-im*k2*i))
+    for i in 1:length(grid)
 
-    plot(Complex(grid1), psi1, label = "Wavefunction for Time Independent Schordinger Equation in One Dimension", xlabel = "x", ylabel = "psi(x)")
-    plot!(Complex(grid2), psi2)
+        if grid[i] < 5
+            psi[i] = Ψ(Bc1[1], Bc1[2], k1, grid[i])
+        
+        # Boundary in the middle 
+        else
+            psi[i] = Ψ(Bc2[1], Bc2[2], k2, grid[i])
+        end
+    end
+
+    mod_psi = [probabilityAmplitude(i) for i in psi]
+    plot(grid, real(psi),  label = "Wavefunction for Time Independent Schordinger Equation in One Dimension", xlabel = "x", ylabel = "psi(x)")
+    #plot!(grid, real(psi))
+    #plot!()
 end
     
     
